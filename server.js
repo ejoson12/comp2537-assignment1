600fec32b164a78ae0c38dab96ede310f4311159
ejoson1 <95837484+ejoson1@users.noreply.github.com>
2023-05-09 16:14:59 -0700

    Added most functionality to app (signup, login, members area)

--- server.js
+++ server.js
@@ -1,56 +1,23 @@
-// Load the necessary modules
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const express = require('express');
-const session = require('express-session');
-
-const port = process.env.PORT || 3000;
-
-// Load the user model
-const User = require('./models/user');
-
-// Create an Express app
 const app = express();
 
-// Set up session middleware
-app.use(session({
-    secret: `${process.env.NODE_SESSION_SECRET}`,
-    resave: false,
-    saveUninitialized: false,
-  }));
+const user = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+dotenv.config();
 
-// Set up middleware to store the user's name in the session
-app.use((req, res, next) => {
-    if (req.session.user) {
-        res.locals.username = req.session.user.name;
-    }
-    next();
-});
+main().catch(err => console.log(err));
 
-// Home page
-app.get('/', (req, res) => {
-    if (req.session.user) {
-        res.send(`Hello, ${req.session.user.name}.<br><a href="/members">Members Area</a><br><a href="/logout">Logout</a>`);
-    } else {
-        res.send('<a href="/signup">Sign up</a><br><a href="/login">Log in</a>');
-    }
-});
+async function main() {
+  try {
+    await mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.lletqsu.mongodb.net/comp2537?retryWrites=true&w=majority`);
 
-//Logout page
-app.get('/logout', (req, res) => {
-    req.session.destroy(() => {
-      res.redirect('/');
+    console.log("connected to db");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('server is running on port 3000');
     });
-});
-
-
-// 404 page
-app.use((req, res) => {
-    res.status(404).send('404');
-});
-app.get('*', (req, res) => {
-    res.status(404).send('Page not found');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+  } catch (err) {
+    console.error("Error connecting to db:", err.message);
+  }
+}
